8496457a449f6f5776ad6bf84d1210c91df972f4
Копылов Владимир <ruwer555@mail.ru>
2025-09-16 17:13:29 +0700

    Картинка

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-from flask import Flask
+from flask import Flask, url_for, request, redirect
+import datetime
 app = Flask(__name__)
 
 @app.route("/web")
 def web():
     return """<!doctype html> 
-        <html> 
-            <body> 
-                <h1>web-сервер на flask<h1> 
-                <a href="/author">author</a>
-            </body> 
-        </html>"""
+    <html> 
+        <body> 
+            <h1>web-сервер на flask<h1> 
+            <a href="/author">author</a>
+        </body> 
+    </html>"""
 
 @app.route("/author")
 def author():
@@ -27,3 +28,15 @@ def author():
         </body> 
     </html>"""
 
+@app.route("/image")
+def image():
+    path=url_for("static", filename="dub.jpg")
+
+    return """<!doctype html> 
+        <html> 
+            <body> 
+                <h1>Дуб<h1>
+                <img src="''' + path + '''">
+                <a href="/author">author</a>
+            </body> 
+        </html>"""
