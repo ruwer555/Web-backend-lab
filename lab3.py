37ccf3b380b9a84bb214e36707dc45b7b3210fd5
Копылов Владимир <ruwer555@mail.ru>
2025-10-15 13:15:05 +0700

    Настройки отображения

--- lab3.py
+++ lab3.py
@@ -69,3 +69,50 @@ def pay():
 def success():
     price = request.cookies.get('price', 0)
     return render_template('lab3/success.html', price=int(price))
+
+
+@lab3.route('/lab3/settings')
+def settings():
+    color = request.args.get('color')
+    background = request.args.get('background')
+    font_size = request.args.get('font_size')
+    font_family = request.args.get('font_family')  # исправлено: было font_size
+    
+    # Получаем текущие значения из куки
+    current_color = request.cookies.get('color')
+    current_background = request.cookies.get('background')
+    current_font_size = request.cookies.get('font_size')
+    current_font_family = request.cookies.get('font_family')
+    
+    if color or background or font_size or font_family:
+        resp = make_response(redirect('/lab3/settings'))
+        if color:
+            resp.set_cookie('color', color)
+        if background:
+            resp.set_cookie('background', background)
+        if font_size:
+            resp.set_cookie('font_size', font_size)
+        if font_family:
+            resp.set_cookie('font_family', font_family)
+        return resp
+    
+    # Рендерим шаблон с текущими значениями
+    return render_template('lab3/settings.html', color=current_color, background=current_background, font_size=current_font_size, font_family=current_font_family)
+
+
+
+
+    
+    
+    
+    
+
+
+
+
+
+
+
+    
+    
+    
