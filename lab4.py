b4c00470cd4866b10c7120f1b5412e4d7d698cf6
Копылов Владимир <ruwer555@mail.ru>
2025-10-28 22:55:27 +0700

    Авторизация

--- lab4.py
+++ lab4.py
@@ -107,7 +107,6 @@ def tree():
     
     operation = request.form.get('operation')
     
-    # Валидация операций
     if operation == 'cut':
         if tree_count > 0: 
             tree_count -= 1
@@ -116,6 +115,20 @@ def tree():
             tree_count += 1
     
     return redirect('/lab4/tree')
+
+@lab4.route('/lab4/login', methods=['GET', 'POST'])
+def login():
+    if request.method == "GET":
+        return render_template('lab4/login.html', authorized=False)
+    login = request.form.get('login')
+    password = request.form.get('password')
+    if login =='alex' and password == '123':
+        return render_template('lab4/login.html', login=login, password= password, authorized=True)
+    error = 'Неверные логин и/или пароль'
+    return render_template('lab4/login.html', error=error, authorized=False)
+    
+    
+    
     
     
     
