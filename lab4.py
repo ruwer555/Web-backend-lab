4e5d3511b2755effa1fab5e36b83b83a888ae593
Копылов Владимир <ruwer555@mail.ru>
2025-10-28 22:26:18 +0700

    POST/Redirect/GET

--- lab4.py
+++ lab4.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect
 lab4 = Blueprint("lab4", __name__)
 
 @lab4.route('/lab4/')
@@ -106,9 +106,10 @@ def tree():
     
     if operation == 'cut':
         tree_count -= 1
-    if operation == 'plant':
+    elif operation == 'plant':
         tree_count += 1
-    return render_template('lab4/tree.html', tree_count=tree_count)
+    return redirect('/lab4/tree')
+
     
     
     
