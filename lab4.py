08c56574391f14ec4007d65b23778831eda45eb6
Копылов Владимир <ruwer555@mail.ru>
2025-10-28 21:39:11 +0700

    Арифметика

--- lab4.py
+++ lab4.py
@@ -5,31 +5,93 @@ lab4 = Blueprint("lab4", __name__)
 def lab():
     return render_template('/lab4/lab4.html')
 
-
 @lab4.route('/lab4/div-form')
 def lab4_div_form():
-    return render_template("/lab4/div-form.html", page_type='form')
+    return render_template("/lab4/div-form.html", current_page='lab4_div')
+
+@lab4.route('/lab4/mult-form')
+def lab4_mult_form():
+    return render_template("/lab4/div-form.html", current_page='lab4_mult')
+
+@lab4.route('/lab4/add-form')
+def lab4_add_form():
+    return render_template("/lab4/div-form.html", current_page='lab4_add')
+
+@lab4.route('/lab4/sub-form')
+def lab4_sub_form():
+    return render_template("/lab4/div-form.html", current_page='lab4_sub')
+
+@lab4.route('/lab4/expo-form')
+def lab4_expo_form():
+    return render_template("/lab4/div-form.html", current_page='lab4_expo')
 
 @lab4.route('/lab4/div', methods=['POST'])
 def lab4_div():
-    errors = {}
     x1 = request.form.get('x1')
     x2 = request.form.get('x2')
-    if x2 == '' or x1 == '':
-        return render_template("/lab4/div-form.html", x1=x1, x2=x2, page_type='form', error='Оба поля должны быть заполнены!')
+    if x1 == '' or x2 == '':
+        return render_template("/lab4/div-form.html", current_page='lab4_div',error='Оба поля должны быть заполнены!')
     x1 = int(x1)
     x2 = int(x2)
     if x2 == 0:
-        errors['x2'] = 'Деление на ноль невозможно!'  
-        return render_template("/lab4/div-form.html", x1=x1, x2=x2, page_type='lab4_div', errors=errors)
-    
+        return render_template("/lab4/div-form.html", x1=x1, x2=x2, current_page='lab4_div',error='Деление на ноль невозможно!')
     result = x1 / x2
-    return render_template("/lab4/div-form.html", x1=x1, x2=x2, result=result, page_type='lab4_div', errors={})
+    return render_template("/lab4/div-form.html", x1=x1, x2=x2, result=result, current_page='lab4_div')
 
+
+@lab4.route('/lab4/mult', methods=['POST'])
+def lab4_mult():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    if x1 == '' or x2 == '':
+        x1 = int(x1) if x1 else 1
+        x2 = int(x2) if x2 else 1
+        result = x1 * x2
+        return render_template("/lab4/div-form.html", x1=x1, x2=x2, result=result, current_page='lab4_mult')
+    x1 = int(x1)
+    x2 = int(x2)
+    result = x1 * x2
+    return render_template("/lab4/div-form.html", x1=x1, x2=x2, result=result,current_page='lab4_mult')
+
+@lab4.route('/lab4/add', methods=['POST'])
+def lab4_add():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    
+    if x1 == '' or x2 == '':
+        x1 = int(x1) if x1 else 0
+        x2 = int(x2) if x2 else 0
+        result = x1 + x2
+        return render_template("/lab4/div-form.html", x1=x1, x2=x2, result=result,current_page='lab4_add')
+    x1 = int(x1)
+    x2 = int(x2)
+    result = x1 + x2
+    return render_template("/lab4/div-form.html", x1=x1, x2=x2, result=result,current_page='lab4_add')
+
+
+@lab4.route('/lab4/sub', methods=['POST'])
+def lab4_sub():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    if x1 == '' or x2 == '':
+        return render_template("/lab4/div-form.html", current_page='lab4_div',error='Оба поля должны быть заполнены!')
+
+    x1 = int(x1)
+    x2 = int(x2)
+    result = x1 - x2
+    return render_template("/lab4/div-form.html", x1=x1, x2=x2, result=result,current_page='lab4_sub')
     
 
-
-
-
-
-
+@lab4.route('/lab4/expo', methods=['POST'])
+def lab4_expo():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    if x1 == '' or x2 == '':
+        return render_template("/lab4/div-form.html", current_page='lab4_div',error='Оба поля должны быть заполнены!')
+    x1 = int(x1)
+    x2 = int(x2)
+    if x1 == 0 and x2 == 0:
+        return render_template("/lab4/div-form.html", current_page='lab4_div',error='Оба числа не могут быть равны нулю!')
+    result = x1 ** x2
+    return render_template("/lab4/div-form.html", x1=x1, x2=x2, result=result,current_page='lab4_expo')
+    
