811b0937530642c8bf9e5b25a4dcefc815cef4ed
Копылов Владимир <ruwer555@mail.ru>
2025-10-28 20:21:07 +0700

    Деление

--- lab4.py
+++ lab4.py
@@ -4,3 +4,32 @@ lab4 = Blueprint("lab4", __name__)
 @lab4.route('/lab4/')
 def lab():
     return render_template('/lab4/lab4.html')
+
+
+@lab4.route('/lab4/div-form')
+def lab4_div_form():
+    return render_template("/lab4/div-form.html", page_type='form')
+
+@lab4.route('/lab4/div', methods=['POST'])
+def lab4_div():
+    errors = {}
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    if x2 == '' or x1 == '':
+        return render_template("/lab4/div-form.html", x1=x1, x2=x2, page_type='form', error='Оба поля должны быть заполнены!')
+    x1 = int(x1)
+    x2 = int(x2)
+    if x2 == 0:
+        errors['x2'] = 'Деление на ноль невозможно!'  
+        return render_template("/lab4/div-form.html", x1=x1, x2=x2, page_type='lab4_div', errors=errors)
+    
+    result = x1 / x2
+    return render_template("/lab4/div-form.html", x1=x1, x2=x2, result=result, page_type='lab4_div', errors={})
+
+    
+
+
+
+
+
+
